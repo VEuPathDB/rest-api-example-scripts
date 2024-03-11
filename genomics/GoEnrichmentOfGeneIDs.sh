576d9fefe8075aa529bd012c2e4e7ecac3632682
Ryan Doherty <tech@conical.org>
2024-03-11 17:18:01 -0400

    refine documentation

--- genomics/GoEnrichmentOfGeneIDs.sh
+++ genomics/GoEnrichmentOfGeneIDs.sh
@@ -14,17 +14,23 @@
 ##
 ################################################################################
 
+#---------------------------------------------------------------
 # script config
+#---------------------------------------------------------------
 
 webappUrl="https://plasmodb.org/plasmo"
 cookieJar="./cookie-jar"
 outputFile="./goEnrichmentResult.tsv"
 
+#---------------------------------------------------------------
 # search config
+#---------------------------------------------------------------
 
 geneIds="[\"PF3D7_1133400\"]" # JSON array (escape double quotes)
 
+#---------------------------------------------------------------
 # analysis config
+#---------------------------------------------------------------
 
 organism="Plasmodium falciparum 3D7"
 ontologies="Biological Process"
@@ -32,19 +38,25 @@ evidenceCodes="[\\\"Computed\\\",\\\"Curated\\\"]" # JSON array (double escapes 
 goSubset="No"
 pValueCutoff="0.05"
 
+
 ################################################################################
 ##
 ##  Succession of CURL commands to download GO Enrichment result
 ##
 ################################################################################
 
-
+#---------------------------------------------------------------
 # establish a guest user and cookies needed to act as that user
-echo "Establishing guest user"
+#---------------------------------------------------------------
+
+echo "Establishing guest user (creates cookie jar)"
 curl -s -S -c $cookieJar \
      $webappUrl/service/users/current > /dev/null
 
+#---------------------------------------------------------------
 # create a dataset param ID containing specified gene IDs
+#---------------------------------------------------------------
+
 echo "Creating input dataset"
 datasetData="{\"sourceType\":\"idList\",\"sourceContent\":{\"ids\":$geneIds}}"
 datasetId=`curl -s -S -b $cookieJar \
@@ -54,7 +66,10 @@ datasetId=`curl -s -S -b $cookieJar \
      $webappUrl/service/users/current/datasets | \
      jq .id`
 
+#---------------------------------------------------------------
 # create a step for this search's results
+#---------------------------------------------------------------
+
 echo "Creating search step"
 stepData="{\"searchName\":\"GeneByLocusTag\",\"searchConfig\":{\"parameters\":{\"ds_gene_ids\":\"${datasetId}\"},\"wdkWeight\":10},\"customName\":\"IDs List\"}"
 stepId=`curl -s -S -b $cookieJar \
@@ -64,7 +79,10 @@ stepId=`curl -s -S -b $cookieJar \
      $webappUrl/service/users/current/steps | \
      jq .id`
 
+#---------------------------------------------------------------
 # create an analysis on this step
+#---------------------------------------------------------------
+
 echo "Creating analysis"
 analysisData="{\"analysisName\":\"go-enrichment\",\"displayName\":\"Gene Ontology Enrichment\",\"parameters\":{\"organism\":\"${organism}\",\"goAssociationsOntologies\":\"${ontologies}\",\"goEvidenceCodes\":\"${evidenceCodes}\",\"goSubset\":\"${goSubset}\",\"pValueCutoff\":\"${pValueCutoff}\"}}"
 analysisId=`curl -s -S -b $cookieJar \
@@ -74,9 +92,11 @@ analysisId=`curl -s -S -b $cookieJar \
      $webappUrl/service/users/current/steps/$stepId/analyses | \
      jq .analysisId`
 
+#---------------------------------------------------------------
 # wait for analysis to complete
-while :
-do
+#---------------------------------------------------------------
+
+while :; do
   echo "Checking analysis job status"
   jobStatus=`curl -s -S -b $cookieJar \
        $webappUrl/service/users/current/steps/$stepId/analyses/$analysisId/result/status | \
@@ -94,14 +114,20 @@ do
   exit 1
 done
 
+#---------------------------------------------------------------
 # download result
+#---------------------------------------------------------------
+
 echo "Results downloaded to $outputFile"
 curl -s -S -b $cookieJar \
      -o $outputFile \
      $webappUrl/service/users/current/steps/$stepId/analyses/$analysisId/resources?path=hiddenGoEnrichmentResult.tsv
 
+#---------------------------------------------------------------
 # clean up cookie jar
-rm ./cookie-jar
+#---------------------------------------------------------------
+
+rm $cookieJar
 
 echo "Done"
 
